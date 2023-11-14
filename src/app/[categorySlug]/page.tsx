6963bea4bb9f4ec3b12737cbb9ce667d7c8b0e78
batos1er <baptiste.menetrat@iut-dijon.u-bourgogne.fr>
2023-11-14 15:49:06 +0100

    3.3 fini

--- src/app/[categorySlug]/page.tsx
+++ src/app/[categorySlug]/page.tsx
@@ -3,20 +3,39 @@ import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { ProductList } from "../../components/product-list";
 import { NextPageProps } from "../../types";
 import { Metadata } from "next";
-const category = PRODUCTS_CATEGORY_DATA[0];
+import { cache } from 'react'
+import prisma from "../../utils/prisma";
+import { notFound } from "next/navigation";
 
 type Props = {
   categorySlug: string;
 };
 
+export const getCategory = cache((categorySlug: string) => prisma.productCategory.findUnique({
+  include: {
+    products: true,
+  },
+  where : {
+    slug: categorySlug,
+  }
+}));
+
 export async function generateMetadata({ params, searchParams} : NextPageProps<Props>) : Promise<Metadata> {
+  const category = await getCategory(params.categorySlug);
+  if (!category){
+    return {}
+  }
   return {
     title: category.name,
     description: `Trouvez votre inspiration avec un vaste choix de boissons Starbucks parmi nos produits ${category.name}`
   }
 }
 
-export default function CategoryPage({params}: NextPageProps<Props>) {
+export default async function CategoryPage({params}: NextPageProps<Props>) {
+  const category = await getCategory(params.categorySlug);
+  if (!category){
+    return notFound(); 
+  }
   return <SectionContainer>
     <BreadCrumbs 
       items={[
